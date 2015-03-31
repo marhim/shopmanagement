0289891dd8d80253be3728ac3580b9ae80c930f8
marvin.himmelmeier <arusaki@gmx-topmail.de>
2015-03-31 13:50:09 +0200

    Added Vaadin Testbench and -source

---
+++ src/test/java/de/david/shopmanagement/test/MainUITest.java
@@ -0,0 +1,15 @@
+package de.david.shopmanagement.test;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by marvin.himmelmeier on 31.03.2015.
+ */
+public class MainUITest {
+
+    @Test
+    public void testAlwaysTrue() {
+        Assert.assertTrue("Das hier gelingt immer", true);
+    }
+}
