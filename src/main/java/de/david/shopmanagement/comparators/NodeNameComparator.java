0b5e8a26455ed8f35e6c9b23a7006b56c5ba3453
marvin.himmelmeier <arusaki@gmx-topmail.de>
2015-08-13 15:23:39 +0200

    Added NodeNameComparator to sort with names from Nodes

---
+++ src/main/java/de/david/shopmanagement/comparators/NodeNameComparator.java
@@ -0,0 +1,17 @@
+package de.david.shopmanagement.comparators;
+
+import de.david.shopmanagement.database.Neo4JConnector;
+import org.neo4j.graphdb.Node;
+
+import java.util.Comparator;
+
+/**
+ * @author Marvin
+ */
+public class NodeNameComparator implements Comparator<Node> {
+    public int compare(Node n1, Node n2) {
+        Neo4JConnector neo4JConnector = Neo4JConnector.getInstance();
+        return ((String) n1.getProperty(neo4JConnector.getNodePropertyName())).compareTo((String) n2.getProperty(neo4JConnector.getNodePropertyName()));
+    }
+
+}
